dc154960d8058236a3a9d8a93cf8b5b104576efd
cabrera-evil <douglascabrera.dev@gmail.com>
2023-05-22 13:51:34 -0600

    Feature: data layer

---
+++ CityTracker/app/src/main/java/com/evildev/citytracker/data/dataDummy.kt
@@ -0,0 +1,13 @@
+package com.evildev.citytracker.data
+
+val city_Model_1 = CityModel(
+    "Santiago",
+    "7.000.000"
+)
+
+val city_Model_2 = CityModel(
+    "Concepción",
+    "1.000.000"
+)
+
+public val cities = mutableListOf<CityModel>(city_Model_1, city_Model_2)
